bffb4546c0859f2ef04e23246dc4d76d770a4991
DalyMagh <magherbymohamedali@gmail.com>
2023-12-06 12:23:37 +0100

    final commit

---
+++ AppBackEnd/Models/Repositories/EmployeeRepository.cs
@@ -0,0 +1,89 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppBackEnd.Models.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+
+    {
+        private readonly AppDbContext appDbContext;
+        public EmployeeRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public async Task<Employee> AddEmployee(Employee employee)
+        {
+            var result = await appDbContext.Employees.AddAsync(employee);
+            await appDbContext.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        public async Task<Employee> DeleteEmployee(int employeeId)
+        {
+            var result = await appDbContext.Employees
+                .FirstOrDefaultAsync(e=> e.EmployeeId == employeeId);
+            if(result!=null)
+            {
+                appDbContext.Employees.Remove(result);
+                await appDbContext.SaveChangesAsync();
+                return result;
+
+            }
+            return null;
+        }
+
+        public async Task<Employee> GetEmployee(int employeeId)
+        {
+            return await appDbContext.Employees
+                .FirstOrDefaultAsync(e => e.EmployeeId == employeeId);
+        }
+
+        public Task<Employee> GetEmployeeByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            return await appDbContext.Employees.ToListAsync();
+        }
+
+        public async Task<Employee> UpdateEmployee(Employee employee)
+        {
+            var result = await appDbContext.Employees
+            .FirstOrDefaultAsync(e => e.EmployeeId == employee.EmployeeId);
+            if (result != null) {
+            result.FirstName = employee.FirstName;
+            result.LastName = employee.LastName;
+            result.Email = employee.Email;
+            result.DateOfBirth = employee.DateOfBirth;
+            result.Gender = employee.Gender;
+            result.DepartmentId = employee.DepartmentId;
+            result.PhotoPath = employee.PhotoPath;
+
+            await appDbContext.SaveChangesAsync();
+            return result;
+        }
+        return null;
+        }
+
+        public async Task<IEnumerable<Employee>> Search(string name, Gender? gender)
+        {
+            IQueryable<Employee> query = appDbContext.Employees;
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                query = query.Where(e => e.FirstName.Contains(name)
+                            || e.LastName.Contains(name));
+            }
+
+            if (gender != null)
+            {
+                query = query.Where(e => e.Gender == gender);
+            }
+
+            return await query.ToListAsync();
+        }
+    }
+}
---
+++ AppBackEnd/Models/Repositories/IDepartmentRepository.cs
@@ -0,0 +1,10 @@
+﻿using EmployeeManagement.Models;
+
+namespace AppBackEnd.Models.Repositories
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetDepartments();
+        Department GetDepartment(int departmentId);
+    }
+}
